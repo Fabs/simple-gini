3eb22325a4bb5c2806cf559d9a30c763b8bf9a92
Fabs <fnascimento@mesosphere.com>
2018-03-30 16:39:32 +0200

    with gini from 2000

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const Gini = require("./output/Gini");
 const closestCountry = require("./lib/gini2000");
 const { flag } = require("country-emoji");
 
+const moon = "🌓";
+const boom = "💥";
+
+const extendedFlag = country => {
+  if (country.name === "Moon") {
+    return moon;
+  }
+
+  if (country.name === "Boon") {
+    return Boon;
+  }
+
+  return flag(country.name);
+};
+
 const gini = values => {
   const giniValue = Gini.gini(values);
   const country = closestCountry(giniValue);
   return {
     value: giniValue,
     country: closestCountry(giniValue),
-    flag: flag(country.name)
+    flag: extendedFlag(country)
   };
 };
 
