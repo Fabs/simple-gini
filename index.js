1b58788669d6260a11446621b8ce7f587cb337e1
Fabs <fnascimento@mesosphere.com>
2018-03-30 03:28:02 +0200

    1.04 with flags

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const Gini = require("./output/Gini");
+const closestCountry = require("./lib/gini2000");
+const { flag } = require("country-emoji");
 
-module.exports = Gini.gini;
+const gini = values => {
+  const giniValue = Gini.gini(values);
+  const country = closestCountry(giniValue);
+  return {
+    value: giniValue,
+    country: closestCountry(giniValue),
+    flag: flag(country.name)
+  };
+};
+
+module.exports = gini;
